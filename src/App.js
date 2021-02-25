122c26ff87eccb1b070d65302c12c3af0f0a233d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-25 13:07:33 -0500

    adds code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  
-  return (
+    return (
     <main>
       <h1>THIS IS WARREN'S SITE NOW!!!!</h1>
       <p>Ha Ha. My site. Give me a bag of money.</p>
